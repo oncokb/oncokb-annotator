1e50dd90560f9372492f1916f816acc807aa0db6
Hongxin Zhang <hongxin@cbio.mskcc.org>
2020-05-15 17:39:10 -0400

    Give option to annotate Gain/Loss CNA events
    Signed-off-by: Hongxin Zhang <hongxin@cbio.mskcc.org>

--- CnaAnnotator.py
+++ CnaAnnotator.py
@@ -10,7 +10,7 @@ log = logging.getLogger('CnaAnnotator')
 def main(argv):
     if argv.help:
         log.info('\n'
-        'CnaAnnotator.py -i <input CNA file> -o <output CNA file> [-p previous results] [-c <input clinical file>] [-s sample list filter] [-t <default tumor type>] [-u oncokb-base-url] [-b oncokb_api_bear_token]\n'
+        'CnaAnnotator.py -i <input CNA file> -o <output CNA file> [-p previous results] [-c <input clinical file>] [-s sample list filter] [-t <default tumor type>] [-u oncokb-base-url] [-b oncokb_api_bear_token] [-z annotate_gain_loss]\n'
         '  Input CNA file should follow the GISTIC output (https://docs.cbioportal.org/5.1-data-loading/data-loading/file-formats#data-file-1)\n'
         '  Essential clinical columns:\n'
         '    SAMPLE_ID: sample ID\n'
@@ -18,6 +18,7 @@ def main(argv):
         '     1) ONCOTREE_CODE in clinical data file\n'
         '     2) ONCOTREE_CODE exist in MAF\n'
         '     3) default tumor type (-t)\n'
+        '  We do not annotate Gain and Loss by default, add -z to include the analysis. See https://github.com/oncokb/oncokb-annotator/issues/51 for more information.\n'
         '  Default OncoKB base url is https://www.oncokb.org')
         sys.exit()
     if argv.input_file == '' or argv.output_file == '' or argv.oncokb_api_bearer_token == '':
@@ -36,7 +37,7 @@ def main(argv):
 
     log.info('annotating %s ...' % argv.input_file)
     processcnagisticdata(argv.input_file, argv.output_file, argv.previous_result_file, argv.default_cancer_type,
-                         cancertypemap, True)
+                         cancertypemap, True, argv.annotate_gain_loss)
 
     log.info('done!')
 
@@ -52,6 +53,7 @@ if __name__ == "__main__":
     parser.add_argument('-t', dest='default_cancer_type', default='cancer', type=str)
     parser.add_argument('-u', dest='oncokb_api_url', default='', type=str)
     parser.add_argument('-b', dest='oncokb_api_bearer_token', default='', type=str)
+    parser.add_argument('-z', dest='annotate_gain_loss', action="store_true", default=False)
     parser.set_defaults(func=main)
 
     args = parser.parse_args()
